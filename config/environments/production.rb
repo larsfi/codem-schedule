9855177a9de11076abbc2c1fe3c2ec6542ef15f2
Lars Fischer <lars.fi@icloud.com>
2015-07-06 14:17:39 -0300

    * project specific changes: json-wrapping deactivated, asset compiling activated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Scheduler::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -51,7 +51,7 @@ Scheduler::Application.configure do
   # Use a different logger for distributed setups.
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
-  # Use a different cache store in production.
+  # Use a differen:t cache store in production.
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
